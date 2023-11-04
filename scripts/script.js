6a7cd22e113c9fa14913b4b593563ca55f58623f
flaneermeneer <98548017+flaneermeneer@users.noreply.github.com>
2023-11-04 22:13:57 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,13 @@
 // JavaScript Document
-console.log("Howdy!");
+console.log("Those that tasted the bite of his sword.. named him.. The Doomslayer");
 
+var caco  = document.querySelector(".caco")
+var btn  = document.querySelector(".btn")
 
+btn.addEventListener('click', function () {
 
+    caco.src = `./images/anim.gif`
 
+    
 
+})
